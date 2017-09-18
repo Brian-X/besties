bf84c131bfdb50c2c7537cef77406ba56bf47ac8
Brian-X <brian@bae-media.com>
2017-09-17 23:31:00 -0500

    adding stuff I put on my server

---
+++ assets/friends.js
@@ -0,0 +1,21 @@
+
+var friends = [
+{"name":"Jimmy Fallon","photo":"http://cdn3.thr.com/sites/default/files/2017/06/jimmy_fallon_gets_a_mustache_screen_shot.jpg","scores":["5","1","4","4","5","1","2","5","4","1"]},
+{"name":"Amy Schumer","photo":"https://media.gq.com/photos/559ecd415f1c7af15a1eeff6/16:9/pass/amy-schumer-gq-01.jpg","scores":["4","2","5","1","3","2","2","1","3","2"]},
+{"name":"Bill O'Reily","photo":"http://www.adweek.com/tvnewser/wp-content/uploads/sites/3/2016/07/OReillyAdweek.jpg","scores":["5","2","2","2","4","1","3","2","5","5"]},
+{"name":"Michael Moore","photo":"https://media.salon.com/2015/12/michael_moore.jpg","scores":["3","3","4","2","2","1","3","2","2","3"]},
+{"name":"Jane Fonda","photo":"https://www.biography.com/.image/t_share/MTE1ODA0OTcxNjI5NTEyMjA1/jane-fonda-9298034-4-402.jpg","scores":["4","3","4","1","5","2","5","3","1","4"]},
+{"name":"Kim Jong Un","https://cdn.shopify.com/s/files/1/0199/9492/products/KimJongUn_theme-political_T6Vendor_Sweatshirt_FrontModel_1024x1024.jpg?v=1503519470","scores":["4","4","2","3","2","2","3","2","4","5"]},
+{"name":"Kim Jong Un in drag","photo":"https://media.giphy.com/media/12juneOf2Rc4ak/giphy.gif","scores":["1","1","1","1","1","1","1","1","1","1"]},
+{"name":"Charlie Sheen","photo":"http://www.billboard.com/files/styles/article_main_image/public/stylus/1028699-charlie-sheen-617-409.jpg","scores":["5","5","5","5","5","5","5","5","5","5"]},
+{"name":"wret","photo":"qwe","scores":["1","1","1","1","1","1","1","1","1","1"]},
+{"name":"test","photo":"test","scores":["3","2","2","1","2","1","1","1","1","1"]},
+{"name":"David","photo":"https://scontent-dft4-1.xx.fbcdn.net/v/t1.0-1/p320x320/16114287_1361156863924060_7489940908640818599_n.jpg?oh=16f59ea5cacf0a09f83febe1c8ae875b&oe=5A15EEC5","scores":["2","3","3","3","3","2","3","3","4","1"]},
+{"name":"David","photo":"https://scontent-dft4-1.xx.fbcdn.net/v/t1.0-1/p320x320/16114287_1361156863924060_7489940908640818599_n.jpg?oh=16f59ea5cacf0a09f83febe1c8ae875b&oe=5A15EEC5","scores":["2","3","3","3","3","2","3","3","4","1"]},
+{"name":"A","photo":"link","scores":["2","3","3","1","3","4","2","3","5","2"]},{"name":"Jorge","photo":"https://pbs.twimg.com/profile_images/875805307750019072/Sn3G-6Hl.jpg","scores":["2","2","3","4","1","2","4","4","1","4"]},{"name":"Jorge","photo":"https://pbs.twimg.com/profile_images/875805307750019072/Sn3G-6Hl.jpg","scores":["2","2","3","4","1","4","4","4","1","4"]},
+{"name":"pam","photo":"https://pbs.twimg.com/profile_images/875805307750019072/Sn3G-6Hl.jpg","scores":["3","2","3","4","1","4","4","4","1","4"]},{"name":"fill murray","photo":"http://fillmurray.com/200/300","scores":["5","4","3","1","3","3","5","3","1","1"]},
+{"name":"gerald","photo":"none","scores":["1","1","1","1","1","1","1","1","1","1"]},
+{"name":"daca","photo":"http://idontseatheporpoiseinthis.weebly.com/uploads/2/6/0/1/26012442/136029_orig.jpg","scores":["2","3","4","5","2","3","3","3","3","2"]}
+];
+
+module.exports = friends;
---
+++ server.js
@@ -0,0 +1,24 @@
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+var PORT = process.env.PORT;
+
+// Expose the public directory to access CSS files
+app.use(express.static(path.join(__dirname, './app/public')));
+
+// Add middleware for parsing incoming request bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+
+// Add the application routes
+require(path.join(__dirname, './assets/apiRoutes'))(app);
+require(path.join(__dirname, './assets/htmlRoutes'))(app);
+
+// Start listening on PORT
+app.listen(PORT, function() {
+  console.log("Besties is on a Mexican Radio: " + PORT);
+});
